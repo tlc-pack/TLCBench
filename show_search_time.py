7c8e6aa61487094940779b475027049165763cd8
Lianmin Zheng <lianminzheng@gmail.com>
2021-01-11 18:54:29 +0000

    update t4

--- show_search_time.py
+++ show_search_time.py
@@ -14,7 +14,7 @@ def get_search_time_autotvm(filename):
         t1 = json.loads(lines[0])['result'][-1]
         t2 = json.loads(lines[-1])['result'][-1]
 
-    return t2 - t1
+    return t2 - t1, len(lines)
 
 
 def get_search_time_autoscheduler(filename):
@@ -23,7 +23,7 @@ def get_search_time_autoscheduler(filename):
         t1 = json.loads(lines[0])['r'][-1]
         t2 = json.loads(lines[-1])['r'][-1]
 
-    return t2 - t1
+    return t2 - t1, len(lines)
 
 
 if __name__ == "__main__":
@@ -34,20 +34,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("-" * 70)
-    print("Search Time (s) | Log file")
+    print("Time (s) | #Line | Log file")
     print("-" * 70)
 
     filenames = glob.glob(os.path.join(args.logdir, "autotvm", "*", "*.kernel.log"))
     filenames.sort()
     for filename in filenames:
-        print("%-15d | %s" % (get_search_time_autotvm(filename), filename[len(args.logdir):]))
+        time, lines = get_search_time_autotvm(filename)
+        print("%8d | %6d | %s" % (time, lines, filename[len(args.logdir):]))
 
     print("")
 
     filenames = glob.glob(os.path.join(args.logdir, "autoscheduler", "*", "*.json"))
     filenames.sort()
     for filename in filenames:
-        print("%-15d | %s" % (get_search_time_autoscheduler(filename), filename[len(args.logdir):]))
+        time, lines = get_search_time_autoscheduler(filename)
+        print("%8d | %6d | %s" % (time, lines, filename[len(args.logdir):]))
 
     print("-" * 70)
 
